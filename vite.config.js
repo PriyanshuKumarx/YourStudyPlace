75743da2620caae81d7dc702fe4065a2b1e39d6f
Priyanshu Kumar <priyanshukumarx7814@gmail.com>
2025-03-27 12:36:45 +0530

    Updated code with improvements

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/YourStudyPlace/", 
   optimizeDeps: {
-    include: ["@fortawesome/react-fontawesome", "@fortawesome/free-solid-svg-icons"],
+    include: [
+      "@fortawesome/react-fontawesome", 
+      "@fortawesome/free-solid-svg-icons"
+    ],
   },
+  build: {
+    outDir: "dist", 
+  }
 });
